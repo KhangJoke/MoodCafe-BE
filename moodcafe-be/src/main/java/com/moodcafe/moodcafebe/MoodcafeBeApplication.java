fe3744ad25447be6df9cd36990f8f6c32872559e
KhangJoke <khangngoc3082005@gmail.com>
2026-09-09 14:36:16 +0700

    first commit

---
+++ moodcafe-be/src/main/java/com/moodcafe/moodcafebe/MoodcafeBeApplication.java
@@ -0,0 +1,13 @@
+package com.moodcafe.moodcafebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodcafeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodcafeBeApplication.class, args);
+    }
+
+}
---
+++ moodcafe-be/src/test/java/com/moodcafe/moodcafebe/MoodcafeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodcafe.moodcafebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodcafeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
